a152d8c9a1e60d6b3cf7323135184232bc21af25
Rohit271298 <rohitsharma.9169@gmail.com>
2024-02-24 17:19:05 +0530

    add

--- functions.js
+++ functions.js
@@ -4,3 +4,21 @@ function add(a,b){
 }
 
 add(5,6);
+
+const avg = function(m1 , m2, m3){
+    let avg = (m1 + m2 + m3)/ 3;
+    console.log(avg);
+
+    return avg;
+
+}
+const result = avg(34, 5467, 24);
+console.log(result);
+
+const percentage =(max, obt) => {
+    let per = (obt / max) * 100;
+    return per;
+}
+
+const percent = percentage(300,250);
+console.log(percent);
